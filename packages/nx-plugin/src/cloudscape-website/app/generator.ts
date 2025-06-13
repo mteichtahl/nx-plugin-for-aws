55b3deead5f4d2e89cc5e9bb3c04bc07ff7c0c1e
agdimech <dimecha@amazon.com>
2025-06-13 16:04:31 +1000

    fix(cloudscape-website): update vite.config.ts to support latest tanstack release
    fix #221

--- packages/nx-plugin/src/cloudscape-website/app/generator.ts
+++ packages/nx-plugin/src/cloudscape-website/app/generator.ts
@@ -313,9 +313,11 @@ export async function tsCloudScapeWebsiteGenerator(
     addDestructuredImport(
       tree,
       viteConfigPath,
-      ['TanStackRouterVite'],
+      ['tanstackRouter'],
       '@tanstack/router-plugin/vite',
     );
+
+    addDestructuredImport(tree, viteConfigPath, ['resolve'], 'path');
 
     addSingleImport(
       tree,
@@ -365,12 +367,39 @@ export async function tsCloudScapeWebsiteGenerator(
               'plugins',
               factory.createArrayLiteralExpression(
                 [
-                  ...pluginsConfig.elements,
                   factory.createCallExpression(
-                    factory.createIdentifier('TanStackRouterVite'),
+                    factory.createIdentifier('tanstackRouter'),
                     undefined,
-                    [],
+                    [
+                      factory.createObjectLiteralExpression([
+                        factory.createPropertyAssignment(
+                          factory.createIdentifier('routesDirectory'),
+                          factory.createCallExpression(
+                            factory.createIdentifier('resolve'),
+                            undefined,
+                            [
+                              factory.createIdentifier('__dirname'),
+                              factory.createStringLiteral('src/routes'),
+                            ],
+                          ),
+                        ),
+                        factory.createPropertyAssignment(
+                          factory.createIdentifier('generatedRouteTree'),
+                          factory.createCallExpression(
+                            factory.createIdentifier('resolve'),
+                            undefined,
+                            [
+                              factory.createIdentifier('__dirname'),
+                              factory.createStringLiteral(
+                                'src/routeTree.gen.ts',
+                              ),
+                            ],
+                          ),
+                        ),
+                      ]),
+                    ],
                   ),
+                  ...pluginsConfig.elements,
                   factory.createCallExpression(
                     factory.createIdentifier('tsconfigPaths'),
                     undefined,
